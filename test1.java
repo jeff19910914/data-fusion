a36b16a72ffff11379ef048d8cfc55cfb3193e02
jeff.zhou <794286504@qq.com>
2017-10-11 23:04:12 +0800

    your message

--- test1.java
+++ test1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class  test1{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		    System.out.println("请输入一个数： ");
+		    System.out.println("请输入一个实数： ");
 			String str = s.next();
 			System.out.println(str);
 
