b192a26386eab0fd3597e74928bd6e5adad0acb7
jeff.zhou <794286504@qq.com>
2017-10-11 22:58:54 +0800

    我提交了一个Java

---
+++ test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class  test1{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		    System.out.println("请输入一个数： ");
+			String str = s.next();
+			System.out.println(str);
+
+
+
+
+	}
+}
