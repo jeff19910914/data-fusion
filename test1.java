d32dd0250d169859f67c95a42b3db367517ee9e3
jeff.zhou <794286504@qq.com>
2017-10-11 23:05:41 +0800

    your message

--- test1.java
+++ test1.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 class  test1{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		    System.out.println("请输入一个实数： ");
-			String str = s.next();
+		    System.out.print("请输入一个实数： ");
+			String str = s.next223();
 			System.out.println(str);
 
 
